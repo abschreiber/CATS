44f24636bf42475f8e0bd8fc6da0268eb376fa6f
Wayne Graham <wayne.graham@virginia.edu>
2014-08-08 11:27:41 -0400

    update lessons_learned.rb
    Adds color examples

--- lessons_learned.rb
+++ lessons_learned.rb
@@ -1,3 +1,12 @@
 #! /usr/bin/env ruby
 
+require_relative 'lib/colors'
 
+puts "this is red".red
+puts "this is blue".blue
+puts "this is green".green
+puts "this is yellow".yellow
+puts "this is purple".purple
+puts "this is cyan".cyan
+puts "this is lightgray".lightgray
+puts "this is white"
