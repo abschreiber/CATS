a91d46551a425dd1f7c910f23d8c2cda5611684b
Wayne Graham <wayne.graham@virginia.edu>
2014-08-08 13:24:41 -0400

    Update start server script

--- start_server.rb
+++ start_server.rb
@@ -1,3 +1,13 @@
 #! /usr/bin/env ruby
 
+user = `whoami`.chomp
+@path = "/Users/#{user}/projects/scriba"
+
+job = fork do
+  exec "#{@path}/bin/rails server"
+end
+
+Process.detach(job)
+
+`open http://localhost:3000`
 
